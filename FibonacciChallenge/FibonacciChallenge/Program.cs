51a00751a350a23ea36ae9bfd6746eef763fdc25
Greg McIntyre <nocolong@gmail.com>
2016-04-08 13:29:50 -0700

    initial commit

---
+++ FibonacciChallenge/FibonacciChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to end the Fibonacci sequence: ");
+            int entry = int.Parse(Console.ReadLine());
+
+            int firstNumber = 0;
+            Console.Write(firstNumber);
+            int secondNumber = 1;
+            Console.Write(secondNumber);
+            int sum = 0;
+
+            while(sum <= entry)
+            {
+                sum = firstNumber + secondNumber;
+                Console.Write(sum);
+                firstNumber = secondNumber;
+                secondNumber = sum;
+
+               
+            }
+            Console.ReadLine();
+        
+        }
+    }
+}
